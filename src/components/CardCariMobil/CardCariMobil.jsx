4c3852af4fc013a6a9df661312d05ce0f57a516a
HarisMI40 <harismaulanaikhsan40@gmail.com>
2023-12-24 17:00:10 +0700

    update hal cariMobil : + get data mobil

--- src/components/CardCariMobil/CardCariMobil.jsx
+++ src/components/CardCariMobil/CardCariMobil.jsx
@@ -1,42 +1,75 @@
-import React from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
 
-const CardCariMobil = ({onActiveHandler, onBlurHandler}) => {
+
+const CardCariMobil = ({onActiveHandler, onBlurHandler, setCar}) => {
+    const [formData, setFormData] = useState({name : "", category : "", price : "", status : ""});
+
+    let [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        let parameterData = Object.fromEntries([...searchParams])
+
+        if(Object.keys(parameterData).length === 0) return // cek jika object ( parameter ) kosong
+
+        const getData = async () => {
+            const res = await axios.get("https://api-car-rental.binaracademy.org/customer/v2/car", { params: parameterData});
+            setCar(res.data)
+        }
+
+        getData();
+
+    }, [])
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setSearchParams(formData);
+
+        axios.get("https://api-car-rental.binaracademy.org/customer/v2/car", { params: formData})
+        .then((res) =>  setCar(res.data))
+        .finally(() => onBlurHandler())
+    }
     return (
-        <div class="shadow p-4 bg-body-tertiary rounded" style={{position:"relative", zIndex:"9999"}}>
-            <Form>
+        <div className="shadow p-4 bg-body-tertiary rounded" style={{position:"relative", zIndex:"9999"}}>
+            <Form onSubmit={submitHandler}>
                 <Row className="mb-3">
                     <Form.Group as={Col} xs={2} controlId="formGridEmail">
                         <Form.Label>Nama Mobil</Form.Label>
-                        <Form.Control size="lg" type="text" placeholder="Ketik nama/tipe mobil" onFocus={onActiveHandler} onBlur={onBlurHandler} />
+                        <Form.Control size="md" type="text" placeholder="Ketik nama/tipe mobil" onFocus={onActiveHandler} onBlur={onBlurHandler} value={formData.name} onChange={ (e) => setFormData({...formData, name : e.target.value})}/>
                     </Form.Group>
 
                     <Form.Group as={Col} xs={3} controlId="formGridEmail">
                         <Form.Label>Kategori</Form.Label>
-                        <Form.Select size="lg" defaultValue="Choose..."  onFocus={onActiveHandler} onBlur={onBlurHandler}>
+                        <Form.Select size="md" onFocus={onActiveHandler} onBlur={onBlurHandler} value={formData.category} onChange={ (e) => setFormData({...formData, category : e.target.value})}>
                             <option>Masukan Kapasitas Mobil</option>
-                            <option>...</option>
+                            <option value="small">2 - 4 Orang</option>
+                            <option value="medium">4 - 6 Orang</option>
+                            <option value="large">6 - 8 Orang</option>
                         </Form.Select>
                     </Form.Group>
 
                     <Form.Group as={Col} xs={3} controlId="formGridEmail">
                         <Form.Label>Harga</Form.Label>
-                        <Form.Select size="lg" defaultValue="Choose..."  onFocus={onActiveHandler} onBlur={onBlurHandler}>
+                        <Form.Select size="md" onFocus={onActiveHandler} onBlur={onBlurHandler} value={formData.price} onChange={ (e) => setFormData({...formData, price : e.target.value})}>
                             <option>Masukan Harga Sewa Per Hari</option>
-                            <option>...</option>
+                            <option value="1212">100.000</option>
+                            <option value="11111">200.000</option>
+                            <option value="300000">300.000</option>
                         </Form.Select>
                     </Form.Group>
 
                     <Form.Group as={Col} xs={2} controlId="formGridEmail">
                         <Form.Label>Status</Form.Label>
-                        <Form.Select size="lg" defaultValue="Choose..."  onFocus={onActiveHandler} onBlur={onBlurHandler}>
-                            <option>Disewa</option>
-                            <option>...</option>
+                        <Form.Select size="md" onFocus={onActiveHandler} onBlur={onBlurHandler} value={formData.status} onChange={ (e) => setFormData({...formData, status : e.target.value})}>
+                            <option value="true">Disewa</option>
+                            <option value="false">Tidak Disewa</option>
                         </Form.Select>
                     </Form.Group>
 
                     <Col className="d-flex align-items-end" xs={2}>
-                        <Button size="lg" variant="success" onFocus={onActiveHandler} onBlur={onBlurHandler}>Cari Mobil</Button>{' '}
+                        <Button size="md" type="submit" variant="success" onFocus={onActiveHandler} onBlur={onBlurHandler}>Cari Mobil</Button>{' '}
                     </Col>
                     {/* </Form.Group> */}
 
