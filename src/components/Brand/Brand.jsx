140cbf7306d794e740ca0998487a5b337fc0a949
HarisMI40 <harismaulanaikhsan40@gmail.com>
2024-03-11 21:26:59 +0700

    update brand pada Header : update link

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -2,7 +2,7 @@ import style from "./Brand.module.css";
 import { Link } from "react-router-dom";
 
 const Brand = () => {
-  return <Link href="/" className={style.brand}></Link>;
+  return <Link to="/" className={style.brand}></Link>;
 };
 
 export default Brand;
