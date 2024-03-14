0bc3b56eb940a5e527190d308b6339a1d9ce7d62
sufyanalvaro <sufyan35@gmail.com>
2024-03-14 11:21:38 +0700

    hit api 200

---
+++ src/services/auth.service.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const login = (data) => {
+  axios
+    .post("https://fakestoreapi.com/auth/login", data)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
